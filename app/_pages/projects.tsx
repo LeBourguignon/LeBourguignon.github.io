bfa088f8909075d51131d9017d490480300e673f
LeBourguignon <63340452+LeBourguignon@users.noreply.github.com>
2024-07-26 16:40:42 +0200

    Add _pages/

---
+++ app/_pages/projects.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { Header } from "../_components/Header";
+import { Spacing } from "../_components/Spacing";
+import { Footer } from "../_components/Footer";
+
+export default function Projects() {
+    return(
+      <main>
+        <Header />
+  
+        <Spacing size="lg"/>
+  
+        <Footer />
+      </main>
+    );
+  }
