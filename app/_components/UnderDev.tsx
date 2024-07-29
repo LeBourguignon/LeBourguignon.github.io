24465de54d964713a23b11d97406863ac76ad40a
LeBourguignon <63340452+LeBourguignon@users.noreply.github.com>
2024-07-29 21:45:14 +0200

    Update UnderDev.tsx

--- app/_components/UnderDev.tsx
+++ app/_components/UnderDev.tsx
@@ -3,8 +3,8 @@ import React from "react";
 export const UnderDev = () => {
     return (
         <div className="flex flex-grow flex-col items-center justify-center gap-4 lg:gap-8">
-            <h1 className="text-4xl font-bold">🚧 En cours de construction 🚧</h1>
-            <p className="text-lg text-muted-foreground">Cette page est actuellement en construction. N&apos;hésitez pas à revenir plus tard !</p>
+            <h1 className="text-center text-4xl font-bold">🚧 En cours de construction 🚧</h1>
+            <p className="text-center text-lg text-muted-foreground">Cette page est actuellement en construction. N&apos;hésitez pas à revenir plus tard !</p>
         </div>
     );
 }   
